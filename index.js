b119190bd9931cada9e5661872d9123ac5fd9c20
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2022-11-06 14:13:26 -0500

    commit tree 0db2d33810851f258692795c5aef25b57d099615

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    function isWin(element, index, array) {
+        return (element.result === "W");
+    };
+    const dbWin = record.find(isWin);
+    if (dbWin === undefined) {
+        return undefined;
+    } else {
+        return dbWin.year;
+    };
+};
